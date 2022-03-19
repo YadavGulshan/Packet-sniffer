706eca0dced3c80b77b428d626026dfba3f95123
YadavGulshan <yadavgulshan542@gmail.com>
2022-03-20 01:39:25 +0530

    Added a command query for setting a device to capture traffic

--- capture/capture.go
+++ capture/capture.go
@@ -5,6 +5,63 @@ import (
 	"github.com/google/gopacket/pcap"
 )
 
+type Options struct {
+	// List of devices user want's to sniff.
+	Devices []string
+	//	Berkeley packet filters
+	//BPFFilter   string
+	//Promiscous  bool
+	SnapshotLen int32
+	Timeout     int
+	//File        string
+	//MaxPackets int
+}
+
+const (
+	defaultTimeOut     = 30
+	defaultSnapshotLen = 1024
+)
+
+// Validate function
+func (opt *Options) Validate() (err error) {
+	if len(opt.Devices) < 1 {
+		err = fmt.Errorf("provide a device to sniff")
+		return
+	}
+
+	// Check if all the devices mentioned exists in the current device.
+	for _, device := range opt.Devices {
+		ok, errX := IsDeviceExists(device)
+		err = errX
+		if !ok {
+			err = fmt.Errorf("device Name with %s does not exists", device)
+			return
+		}
+	}
+	if opt.SnapshotLen == 0 {
+		opt.SnapshotLen = defaultSnapshotLen
+	}
+	if opt.Timeout == 0 {
+		opt.Timeout = defaultTimeOut
+	}
+	return
+}
+
+func IsDeviceExists(name string) (ok bool, err error) {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		return false, err
+	}
+	for _, device := range devices {
+		if name == device.Name {
+			println("device exists!")
+			ok = true
+			return ok, nil
+		}
+	}
+	return false, nil
+}
+
 func ListDevices() (err error) {
 	devices, err := pcap.FindAllDevs()
 	if err != nil {
--- cmd/capture.go
+++ cmd/capture.go
@@ -1,1 +1,30 @@
 package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/yadavgulshan/packet-analyzer/capture"
+)
+
+var captureCmd = &cobra.Command{
+	Use:   "capture",
+	Short: "Capture packets",
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		device, err := cmd.Flags().GetStringArray("devices")
+		if err != nil {
+			return err
+		}
+		captureOpt := &capture.Options{
+			Devices: device,
+		}
+
+		err = captureOpt.Validate()
+		if err != nil {
+			return
+		}
+		return
+	}}
+
+func init() {
+	rootcmd.AddCommand(captureCmd)
+	captureCmd.Flags().StringArrayP("devices", "d", []string{}, "Devices to capture")
+}
